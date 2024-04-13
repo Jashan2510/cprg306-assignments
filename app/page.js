9f9727c35ef97bfa478d192306cb6aeb09249b33
Jashan2510 <139100933+Jashan2510@users.noreply.github.com>
2024-04-13 01:06:51 -0600

    update page.js

--- app/page.js
+++ app/page.js
@@ -7,31 +7,32 @@ export default function Page() {
       <h1 className="text-3xl font-bold mb-8">Shopping List</h1>
       
       {/* Navigation to weekly projects */}
-      <div className="flex flex-col items-center justify-center bg-blue-200 p-10">
+      <div className="flex flex-col items-center justify-center bg-red-200 p-10">
         <h2 className="text-4xl font-mono font-bold text-black mb-5">CPRG 306 Assignments</h2>
         
-        <Link href="/week-2">
-            Click here to see my Project!
+        <Link href="/week-2" className="text-current no-underline hover:text-black hover:underline">
+            Click here to see my Project part 2!
         </Link>
-        <Link href="/week-3">
+
+        <Link href="/week-3" className="text-current no-underline hover:text-black hover:underline">
             Click here to see my Project part 3!
         </Link>
-        <Link href="/week-4">
+        <Link href="/week-4" className="text-current no-underline hover:text-black hover:underline">
             Click here to see my Project part 4!
         </Link>
-        <Link href="/week-5">
+        <Link href="/week-5" className="text-current no-underline hover:text-black hover:underline">
             Click here to see my Project part 5!
         </Link>
-        <Link href="/week-6">
+        <Link href="/week-6" className="text-current no-underline hover:text-black hover:underline">
             Click here to see my Project part 6!
         </Link>
-        <Link href="/week-7">
+        <Link href="/week-7" className="text-current no-underline hover:text-black hover:underline">
             Click here to see my Project part 7!
         </Link>
-        <Link href="/week-8">
+        <Link href="/week-8" className="text-current no-underline hover:text-black hover:underline">
             Click here to see my Project part 8!
         </Link>
-        <Link href="/week-10">
+        <Link href="/week-10" className="text-current no-underline hover:text-black hover:underline">
             Click here to see my Project part 10!
         </Link>
       </div>
